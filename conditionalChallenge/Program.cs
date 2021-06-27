815c5a3a6b58974c3243a8527d3dba9119cde2a6
CurleyT <curleyt727@gmail.com>
2021-06-27 09:38:33 -0400

    Initial Commit for Package Express

---
+++ conditionalChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace conditionalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 and 100:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if(input < 24)
+            {
+                Console.WriteLine("Your number is less than 24.");
+            } else if (input > 24 && input < 50) 
+            {
+                Console.WriteLine("Your number is greater than 24 but less than 50.");
+            } else
+            {
+                Console.WriteLine("Your number is 50 or greater.");
+            }
+        }
+    }
+}
---
+++ packageExpress/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace packageExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x = true;
+            Console.WriteLine("Welcome to Package Express. Please follow the instruction below.");
+            while (x == true)
+            {
+                Console.WriteLine("What is the package weight? ");
+                double weight = Convert.ToDouble(Console.ReadLine());
+
+                if (weight > 50.0)
+                {
+                    Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                    Console.ReadLine();
+                    System.Environment.Exit(0);
+                }
+
+                Console.WriteLine("What is the package width?");
+                double width = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("What is the package height?");
+                double height = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("What is the package length?");
+                double length = Convert.ToDouble(Console.ReadLine());
+
+                if ((height + length + width) > 50.0)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                    Console.ReadLine();
+                    System.Environment.Exit(0);
+                }
+
+                double quote = ((height * width * length) * weight) / 100;
+                Console.WriteLine("Your total for shipping this package is: " + quote);
+                Console.WriteLine("Would you like to ship another package? Please enter Yes or No.");
+                string repeatProg = Console.ReadLine();
+                x = String.Equals(repeatProg, "Yes", StringComparison.OrdinalIgnoreCase);
+
+            }
+            Console.WriteLine("Thank you!");
+            Console.ReadLine();
+        }
+    }
+}
+
