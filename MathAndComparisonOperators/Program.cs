7cadf9e5034d6f6471f645229d4be89833291f12
CurleyT <curleyt727@gmail.com>
2021-06-27 08:30:02 -0400

    Initial Commit for Math and Operators Assignment
    Included the ability to choose multiple employees.

--- MathAndComparisonOperators/Program.cs
+++ MathAndComparisonOperators/Program.cs
@@ -23,9 +23,24 @@ namespace MathAndComparisonOperators
             //Console.WriteLine(quotient);
             //Console.ReadLine();
 
-            int remainder = 11 % 2;
-            Console.WriteLine(remainder);
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            int roomTemperature = 70;
+            int currentTemperature = 70;
+
+            //bool isWarm = currentTemperature >= roomTemperature;
+            bool isWarm = currentTemperature != roomTemperature;
+
+            Console.WriteLine(isWarm);
             Console.ReadLine();
+
+
 
         }
     }
