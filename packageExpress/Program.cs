1139487908181b3a102d288e14351de882c74057
CurleyT <curleyt727@gmail.com>
2021-06-27 09:43:15 -0400

    Added comments

--- packageExpress/Program.cs
+++ packageExpress/Program.cs
@@ -6,37 +6,35 @@ namespace packageExpress
     {
         static void Main(string[] args)
         {
-            bool x = true;
-            Console.WriteLine("Welcome to Package Express. Please follow the instruction below.");
-            while (x == true)
+            bool x = true;                                                                              //Creating a boolean for the while loop
+            Console.WriteLine("Welcome to Package Express. Please follow the instruction below.");  
+            while (x == true)                                                                           //Start of While Loop to repeat program
             {
                 Console.WriteLine("What is the package weight? ");
-                double weight = Convert.ToDouble(Console.ReadLine());
+                double weight = Convert.ToDouble(Console.ReadLine());                                   //Requesting Package weight and converting it to a double
 
-                if (weight > 50.0)
+                if (weight > 50.0)                                                                      //If weight is greater than 50, exit the loop
                 {
                     Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
-                    Console.ReadLine();
-                    System.Environment.Exit(0);
+                    break;
                 }
 
-                Console.WriteLine("What is the package width?");
+                Console.WriteLine("What is the package width?");                                        //Taking package width as a double
                 double width = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("What is the package height?");
+                Console.WriteLine("What is the package height?");                                       //Taking package height as a double
                 double height = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("What is the package length?");
+                Console.WriteLine("What is the package length?");                                       //Taking package length as a double
                 double length = Convert.ToDouble(Console.ReadLine());
 
-                if ((height + length + width) > 50.0)
+                if ((height + length + width) > 50.0)                                                   //If sum of dimensions is greater than 50, exit the loop
                 {
                     Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
-                    Console.ReadLine();
-                    System.Environment.Exit(0);
+                    break;
                 }
 
-                double quote = ((height * width * length) * weight) / 100;
+                double quote = ((height * width * length) * weight) / 100;                              //Determining the $$ based on dimensions provided and displaying them
                 Console.WriteLine("Your total for shipping this package is: " + quote);
-                Console.WriteLine("Would you like to ship another package? Please enter Yes or No.");
+                Console.WriteLine("Would you like to ship another package? Please enter Yes or No.");   //Requesting input to enter another package or end the loop
                 string repeatProg = Console.ReadLine();
                 x = String.Equals(repeatProg, "Yes", StringComparison.OrdinalIgnoreCase);
 
