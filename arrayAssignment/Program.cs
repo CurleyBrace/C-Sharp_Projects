eba6df32143d04bfde77b6c600a6cfd72a416655
CurleyT <curleyt727@gmail.com>
2021-06-29 12:01:54 -0400

    Initial Commit of Array Assignment

---
+++ arrayAssignment/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace arrayAssignment
+{
+    class Program
+    {
+        //These are the pre-determined arrays that will allow the user to select from
+        public string[] stringArray = new string[10] { "Amber", "Barbara", "Bennett", "Diona", "Jean", "Lisa", "Michael", "Sarah", "Thomas", "William" };
+        public int[] intArray = new int[10] {2, 356, 4302, 6774, 9438, 10213, 14049, 19320, 21304, 22504};
+        public List<string> stringList = new List<string>() { "This", "is", "A", "String", "List", "Full", "of", "Strings", "To", "Display" };
+        public static bool breakString = false;
+
+
+        static void Main(string[] args)
+        {
+            bool x = true;                                                                                   //This is a Boolean that controls the while loop that repeats the program
+            int displayInput = 0;                                                                            //This is the input variable to determine the index of the displayed array/list
+            string sameArray = "no";
+                                                                                                             //Text Prompting the user with instructions
+            Console.WriteLine("Welcome. There are three different examples of Array/Lists for you to search through.");
+            Console.WriteLine("You may enter 0-9 to to display and of their held values when prompted.");
+            Console.WriteLine("There is a feature in which you are able to go back through and select variables multiple times per index.");
+            Console.WriteLine("If you'd like to turn off extra prompts, type \"Break\" anytime you are prompted to go back through an array.");
+
+
+            while (x == true)                                                                                //Loop to allow the user to go back through the string array if they want
+            {
+                Console.WriteLine("\nOf the String Array, which index would you like to display?");          //Asking for an index and converting it to int
+                displayInput = Convert.ToInt32(Console.ReadLine());
+
+                arrayPrint(displayInput, 0);                                                                 //Sending the input and a value of 0 to determine the string type based on pre-set parameters
+                if (Program.breakString != true) { x = loopArrays(); }                                       //Asking the user if they'd like to repeat the array and returning the value
+            }
+
+            x = true;                                                                                        //Resetting the While Loop Boolean
+            if (Program.breakString != true) { sameArray = keepIndex(); }
+
+            while (x == true)                                                                                //Loop to allow the user to go back through the string array if they want
+            {
+                if (String.Equals(sameArray, "no", StringComparison.OrdinalIgnoreCase))                      //If they wanted to input a different index, prompts the user 
+                {
+                    Console.WriteLine("\nOf the Integer Array, which index would you like to display?");     //Asking for an index and converting it to int
+                    displayInput = Convert.ToInt32(Console.ReadLine());                                                                      
+                }
+                sameArray = "no";                                                                            //Changing Same Array to No so that if you decide to continue entering numbers it will loop through the previous if statement.
+
+                arrayPrint(displayInput, 1);                                                                 //Sending the input and a value of 1 to determine the string type based on pre-set parameters
+                if (Program.breakString != true) { x = loopArrays(); }                                       //Asking the user if they'd like to repeat the array and returning the value
+                else { x = false; }
+            }
+
+            x = true;                                                                                        //Resetting the While Loop Boolean
+            if (Program.breakString != true) { sameArray = keepIndex(); }
+
+            while (x == true)                                                                                //Loop to allow the user to go back through the string array if they want
+            {
+                if(String.Equals(sameArray, "no", StringComparison.OrdinalIgnoreCase))                       //If they wanted to input a different index, prompts the user 
+                {
+                    Console.WriteLine("\nOf the String List, which index would you like to display?");       //Asking for an index and converting it to int
+                    displayInput = Convert.ToInt32(Console.ReadLine());
+                }
+                sameArray = "no";                                                                            //Changing Same Array to No so that if you decide to continue entering numbers it will loop through the previous if statement.
+
+                arrayPrint(displayInput, 2);                                                                 //Sending the input and a value of 2 to determine the string type based on pre-set parameters
+                if (Program.breakString != true) { x = loopArrays(); }                                       //Asking the user if they'd like to repeat the array and returning the value
+                else { x = false; }
+            }
+            Console.WriteLine("\nThanks for checking out my program!");
+            Console.ReadLine();                                                                              //Ending Statement for the Program
+        }
+
+
+
+        public static void arrayPrint(int indexNum, int arrayNum)                                            //Seperate Function that will check the input and repeat until a valid input has been entered
+        {
+            Program arrayRef = new Program();                                                                //In order to reference the arrays, an object of the program class has been created
+            string arrayDisplay = "";                                                                        //Variable to hold whatever is in the index selected
+
+            while (indexNum > 9 || indexNum < 0)                                                             //While Loop to check that the entered index is within the proper parameters
+            {
+                Console.WriteLine("\nThat is not a valid input. Please choose any number between 0 and 10.");
+                indexNum = Convert.ToInt32(Console.ReadLine());                                              //If the input is not 0-10, repeats until a valid number is entered
+            }
+                                                                                                             //Nested Ternary Statement for assigning holding variable to either string array, int array, or string list
+            arrayDisplay = arrayNum == 2 ? arrayRef.stringList[indexNum] :                                   //Assigning String List to Holding Var
+                            arrayNum == 1 ? Convert.ToString(arrayRef.intArray[indexNum]) :                  //Assigning Int Array to Holding Var
+                             arrayNum == 0 ? arrayRef.stringArray[indexNum] : "null";                        //Assigning String Array to Holding Var. Will return Null if none are returned.
+            Console.WriteLine("The object in the index "+indexNum+" is "+arrayDisplay+".");                                                                 //Prints the desired result that was assigned to the holding var
+        }
+
+
+
+        public static bool loopArrays()                                                                      //A function that asks the user if they'd like to continue through the loop and select another index
+        {
+            bool x = true;                                                                                   //Creating a variable to return
+            string loopClause = "";                                                                          //Holding variable for input
+
+            Console.WriteLine();                                                                             //Requesting input to continue through or exit to another array selection
+            Console.WriteLine("Would you like to choose another index of the same array/list? Yes/No\nYou may type \"Break\" here to disable extra prompts.");  
+            loopClause = Console.ReadLine();                                                    
+            
+            if (String.Equals(loopClause, "break", StringComparison.OrdinalIgnoreCase)) {                    //Statement reading if there is a break or if it's an expected outcome
+                Program.breakString = true;                                                                  //If break is the input, it will change the global var to true and set X to false to exit the loop in Main
+                x = false;
+            } else { 
+                x = String.Equals(loopClause, "yes", StringComparison.OrdinalIgnoreCase);                    //Checking input, will return true if "yes" is entered without case sensitivity
+            }
+
+            return x;                                                                                        //Returning true or false depending on the string comparison above
+        }
+
+
+
+        public static string keepIndex()                                                                     //Asking if they'd like to return the same index in the next Array
+        {
+            Console.WriteLine("\nWould you like to display the same index for the Integer Array? Yes or No."); 
+            string sameInput = Console.ReadLine();
+            return sameInput;
+        }
+    }
+}
---
+++ stringAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace stringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOne = "Sara";                    //Creating Strings to Concatenate
+            string nameTwo = "Jacobs";                  
+            string nameThree = "Michael";               //Creating Strings to Concatenate
+            string nameFour = "Johnson";                
+            string nameFive = "Timothy";                //Creating Strings to Concatenate
+            string nameSix = "Williams";
+
+            string firstConc = nameOne +" "+ nameTwo;       //Concatenating The first two strings together
+            string secondConc = nameThree +" "+ nameFour;   //Concatenating The second two strings together
+            string thirdConc = nameFive +" "+ nameSix;      //Concatenating The third two strings together
+
+            Console.WriteLine(firstConc);               //Printing the first concatenation
+            Console.WriteLine(secondConc);              //Printing the first concatenation
+            Console.WriteLine(thirdConc.ToUpper());               //Printing the first concatenation
+
+            StringBuilder sb = new StringBuilder();     //Creating a StringBuilder object
+            sb.AppendLine(nameOne + "'s last name is " + nameTwo + ". Their favorite food is pizza!");      //Appending three lines to this Stringbuilder using strings we've already created.
+            sb.AppendLine(nameThree + "'s last name is " + nameFour + ". Their favorite food is pasta!");
+            sb.AppendLine(nameFive + "'s last name is " + nameSix + ". Their favorite food is sushi!");
+            sb.AppendLine(firstConc + " really enjoys going to the beach.");
+            sb.AppendLine(secondConc + " really likes to stay home and play video games. ");
+            sb.AppendLine(thirdConc + " loves to play sports with their friends. ");
+
+            Console.WriteLine(sb);                      //Outputting the entire StringBuilder object
+            Console.ReadLine();
+
+        }
+    }
+}
