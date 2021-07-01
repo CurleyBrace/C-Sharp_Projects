e0acfb28ea7163117f4a463b0d02f10b16efc6bc
CurleyT <curleyt727@gmail.com>
2021-07-01 18:21:38 -0400

    Initial Commit for MethodClassSub

---
+++ methodClassAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace methodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathClass mathOp = new mathClass();                     //Creating object MathOp
+
+            mathOp.displayInt(numOne : 15, numTwo : 32);            //Calling MathOps' method to displayInt to multiply 15 times 12, and display the number 32
+
+            Console.ReadLine();                                     //End Line for Program
+        }
+    }
+}
---
+++ methodClassAssignment/mathClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methodClassAssignment
+{
+    class mathClass
+    {
+        public void displayInt(int numOne, int numTwo)      //Requires two integers
+        {
+            int product = numOne * 12;                  //Math operation on first integer
+
+            Console.WriteLine(numTwo);                  //Display second integer on screen
+        }
+
+    }
+}
