6bb3a4c812c1c015292123aaf46444a70920c1e6
CurleyT <curleyt727@gmail.com>
2021-07-01 17:52:41 -0400

    Initial commit for method assignment

---
+++ mainMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace mainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathClass mathOp = new mathClass();                             //Creating new object mathOp
+
+            Console.WriteLine("Please enter an number.");                   //Asking and taking an input, converting to an int
+            int userInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(mathOp.math(userInt));                        //Calling MathOp to perform the math function with the int parameter
+
+            Console.WriteLine("\nPlease enter a number.");                  //Asking and taking an input, converting to an decimal
+            decimal userDec = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(mathOp.math(userDec));                        //Calling MathOp to perform the math function with the decimal parameter
+
+            Console.WriteLine("\nPlease enter a number.");                  //Asking and taking an input
+            string userStr = Console.ReadLine();
+            Console.WriteLine(mathOp.math(userStr));                        //Calling MathOp to perform the math function with the string parameter
+        }
+    }
+}
---
+++ mainMethodAssignment/mathClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mainMethodAssignment
+{
+    class mathClass
+    {
+        public int math(int userInput)
+        {
+            int product = userInput * 15;                   //Will take an int and retun the input multiplied by 15
+            return product;
+        }
+
+        public decimal math(decimal userInput)
+        {
+            decimal divide = userInput / 15;               //Will take a decimal and retun the input divided by 15
+            return divide;
+        }
+
+        public int math(string userInput)
+        {
+            int userNum = Convert.ToInt32(userInput);     //Will take a string and return the input multiplied by itsself
+            int squared = userNum * userNum;
+            return squared; 
+        }
+    }
+}
