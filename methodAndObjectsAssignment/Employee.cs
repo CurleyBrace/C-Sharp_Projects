f73bb9b4f32a37ebcb64fc80c47fd0c947c6a36c
CurleyT <curleyt727@gmail.com>
2021-07-02 16:52:16 -0400

    Fixed Comments

--- methodAndObjectsAssignment/Employee.cs
+++ methodAndObjectsAssignment/Employee.cs
@@ -7,7 +7,7 @@ namespace methodAndObjectsAssignment
     public class Employee : Person, IQuittable          //Inheriting from Person
     {
 
-        public void Quit(Person person)
+        public void Quit(Person person)         //Calling the method Quit from IQuittable with the Parameters sent from Program.cs
         {
             Console.WriteLine("Thank you for working with us " + person.FirstName + " " + person.LastName + ".");
         }
