099e2305496feb5c73bc524f36f488bad1e84962
CurleyT <curleyt727@gmail.com>
2021-07-02 15:45:12 -0400

    Initial commit for classSubAssignment

---
+++ classSubmissionAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace classSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathClass mathOp = new mathClass();                                 //Creating new mathClass
+            int result;                                                         //Variable to take the output from mathClass
+
+            Console.WriteLine("Please enter a number");                         //Asking for int from user
+            int intInput = Convert.ToInt32(Console.ReadLine());
+
+            mathOp.voidMethod(intInput, out result);                            //Sending first input and outputing the result to 'result'
+            Console.WriteLine(result + " divided by 2 is " + result);
+
+            Console.WriteLine("\nPlease a second number.");                      //Asking for int from user
+            int intInputTwo = Convert.ToInt32(Console.ReadLine());
+
+            mathOp.voidMethod(intInput, intInputTwo, out result);                //Sending both inputs and outputing the result to 'result'
+            Console.WriteLine(intInput + " multiplied by " + intInputTwo + " is " + result);
+
+            staticClass.text = "\nThis class is static. ";                       //Setting the value of text in staticClass
+            Console.WriteLine(staticClass.text);                                 //Outputting from staticClass.Text
+        }
+    }
+}
---
+++ classSubmissionAssignment/mathClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classSubmissionAssignment
+{
+    class mathClass
+    {
+        public void voidMethod(int input, out int result)                   //First Void Method, outputs an int and divides by 2
+        {
+            result = input / 2;                                             
+        }
+
+        public void outPutParam(out int divNum, out int result)             //Method with output parameters
+        {
+            divNum = 5;                                             
+            result = 10;
+        }
+
+        public void voidMethod(int inputOne, int inputTwo, out int result)   //Overloaded method
+        {
+            result = inputOne * inputTwo;                                    //Multiplying the two inputs and assigning the output to the result
+        }
+    }
+
+    public static class staticClass                                           //Static class
+    {
+        public static string text;                                            //Giving the static class a value so that we can display it when it changes
+    }
+}
