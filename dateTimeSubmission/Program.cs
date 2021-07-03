e9613ddcb0e09c1f3eef1cb53fa7477e7443d58d
CurleyT <curleyt727@gmail.com>
2021-07-03 08:20:52 -0400

    DateTime Submission Initial Commit

---
+++ dateTimeSubmission/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);                    //Printing current time
+
+            Console.WriteLine("\nPlease enter a number. ");     //Asking for an input and converting it to integer
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            DateTime inputTime = DateTime.Now.AddHours(input);  //Taking input and adding it to the current datetime in the hours parameter and then displaying
+            Console.WriteLine(inputTime);
+            
+        }
+    }
+}
