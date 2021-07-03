c0a881675cecfde36e25cdf9dd6c53a65757724e
CurleyT <curleyt727@gmail.com>
2021-07-03 03:14:07 -0400

    StructAssignment initial Commit

---
+++ structAssignment/Number.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace structAssignment
+{
+    public struct Number                    //Creating new Struct Number
+    {
+        public int Amount { get; set; }     //Giving it an int propery of Amount
+    }
+}
---
+++ structAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace structAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number numOne = new Number();           //Creating new Number numOne
+            numOne.Amount = 5;                      //Setting the Number.Amount to 5
+
+            Console.WriteLine(numOne.Amount);       //Displaying Number.Amount
+            Console.ReadLine();
+        }
+    }
+}
